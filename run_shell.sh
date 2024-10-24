443c923598fcf505d8b553e7fdf0d8f14512d361
unknown <funnygaming998@gmail.com>
2024-10-24 20:49:27 +0700

    Menambahkan File baru subprogram

--- run_shell.sh
+++ run_shell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "---baris ini adalah shell script AyubAndrean yang akan di jalankan---"
+echo "---baris ini adalah shell script AyubAndre yang akan di jalankan---"
 python3 hallo.py
 
 
